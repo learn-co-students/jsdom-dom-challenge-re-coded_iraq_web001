ae35921257881014edbf442356a1fd803023ef0f
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-07-01 12:16:46 +0000

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,9 @@
+
+
+let add = document.querySelector('#plus')
+let subtract = document.querySelector('#minus')
+let num = document.querySelector('#counter')
+
+add.addEventListener('clicl',()=>{
+  h1.value = presentInt(h1.value)+1
+})
